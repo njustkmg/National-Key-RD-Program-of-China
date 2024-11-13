93316690628b26bed5e73b0f1c72f32d56f43cba
njustkmg <zhguan@njust.edu.cn>
2024-11-13 10:23:14 +0800

    Add files via upload

---
+++ ViTAE-main/segmentation/nets/ViTAE_Window_NoShift/base_model.py
@@ -0,0 +1,334 @@
+from functools import partial
+import torch
+import torch.nn as nn
+from timm.models.layers import trunc_normal_
+import numpy as np
+from torch.nn.functional import instance_norm
+from torch.nn.modules.batchnorm import BatchNorm2d
+from .NormalCell import NormalCell
+from .ReductionCell import ReductionCell
+import torch.nn.functional as F
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, inter_channel=32, out_channels=48, img_size=None):
+        self.img_size = img_size
+        self.inter_channel = inter_channel
+        self.out_channel = out_channels
+        super().__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, inter_channel, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(inter_channel),
+            nn.ReLU(inplace=True)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(inter_channel, out_channels, kernel_size=3, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+        self.conv3 = nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        x = self.conv3(self.conv2(self.conv1(x)))
+        b, c, h, w = x.shape
+        x = x.permute(0, 2, 3, 1).reshape(b, h*w, c)
+        return x
+
+    def flops(self, ) -> float:
+        flops = 0
+        flops += 3 * self.inter_channel * self.img_size[0] * self.img_size[1] // 4 * 9
+        flops += self.img_size[0] * self.img_size[1] // 4 * self.inter_channel
+        flops += self.inter_channel * self.out_channel * self.img_size[0] * self.img_size[1] // 16 * 9
+        flops += self.img_size[0] * self.img_size[1] // 16 * self.out_channel
+        flops += self.out_channel * self.out_channel * self.img_size[0] * self.img_size[1] // 16
+        return flops
+
+class BasicLayer(nn.Module):
+    def __init__(self, img_size=224, in_chans=3, embed_dims=64, token_dims=64, downsample_ratios=4, kernel_size=7, RC_heads=1, NC_heads=6, dilations=[1, 2, 3, 4],
+                RC_op='cat', RC_tokens_type='performer', NC_tokens_type='transformer', RC_group=1, NC_group=64, NC_depth=2, dpr=0.1, mlp_ratio=4., qkv_bias=True, 
+                qk_scale=None, drop=0, attn_drop=0., norm_layer=nn.LayerNorm, class_token=False, gamma=False, init_values=1e-4, SE=False, window_size=7, relative_pos=False):
+        super().__init__()
+        self.img_size = img_size
+        self.in_chans = in_chans
+        self.embed_dims = embed_dims
+        self.token_dims = token_dims
+        self.downsample_ratios = downsample_ratios
+        self.out_size = self.img_size // self.downsample_ratios
+        self.RC_kernel_size = kernel_size
+        self.RC_heads = RC_heads
+        self.NC_heads = NC_heads
+        self.dilations = dilations
+        self.RC_op = RC_op
+        self.RC_tokens_type = RC_tokens_type
+        self.RC_group = RC_group
+        self.NC_group = NC_group
+        self.NC_depth = NC_depth
+        self.relative_pos = relative_pos
+        if RC_tokens_type == 'stem':
+            self.RC = PatchEmbedding(inter_channel=token_dims//2, out_channels=token_dims, img_size=img_size)
+        elif downsample_ratios > 1:
+            self.RC = ReductionCell(img_size, in_chans, embed_dims, token_dims, downsample_ratios, kernel_size,
+                            RC_heads, dilations, op=RC_op, tokens_type=RC_tokens_type, group=RC_group, gamma=gamma, init_values=init_values, SE=SE, relative_pos=relative_pos, window_size=window_size)
+        else:
+            self.RC = nn.Identity()
+        self.NC = nn.ModuleList([
+            NormalCell(token_dims, NC_heads, mlp_ratio=mlp_ratio, qkv_bias=qkv_bias, qk_scale=qk_scale, drop=drop, attn_drop=attn_drop,
+                       drop_path=dpr[i] if isinstance(dpr, list) else dpr, norm_layer=norm_layer, class_token=class_token, group=NC_group, tokens_type=NC_tokens_type,
+                       gamma=gamma, init_values=init_values, SE=SE, img_size=img_size // downsample_ratios, window_size=window_size, shift_size=0, relative_pos=relative_pos)
+        for i in range(NC_depth)])
+
+    def forward(self, x):
+        x = self.RC(x)
+        for nc in self.NC:
+            x = nc(x)
+        return x
+
+class MLP(nn.Module):
+    """
+    Linear Embedding
+    """
+    def __init__(self, input_dim=2048, embed_dim=768):
+        super().__init__()
+        self.proj = nn.Linear(input_dim, embed_dim)
+
+    def forward(self, x):
+        x = x.flatten(2).transpose(1, 2)
+        x = self.proj(x)
+        return x
+
+class ConvModule(nn.Module):
+    def __init__(self, c1, c2, k=1, s=1, p=0, g=1, act=True):
+        super(ConvModule, self).__init__()
+        self.conv   = nn.Conv2d(c1, c2, k, s, p, groups=g, bias=False)
+        self.bn     = nn.BatchNorm2d(c2, eps=0.001, momentum=0.03)
+        self.act    = nn.ReLU() if act is True else (act if isinstance(act, nn.Module) else nn.Identity())
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def fuseforward(self, x):
+        return self.act(self.conv(x))
+
+class SegFormerHead(nn.Module):
+    """
+    SegFormer: Simple and Efficient Design for Semantic Segmentation with Transformers
+    """
+
+    def __init__(self, num_classes=20, in_channels=[32, 64, 160, 256], embedding_dim=768, dropout_ratio=0.1):
+        super(SegFormerHead, self).__init__()
+        c1_in_channels, c2_in_channels, c3_in_channels, c4_in_channels = in_channels
+
+        self.linear_c4 = MLP(input_dim=c4_in_channels, embed_dim=embedding_dim)
+        self.linear_c3 = MLP(input_dim=c3_in_channels, embed_dim=embedding_dim)
+        self.linear_c2 = MLP(input_dim=c2_in_channels, embed_dim=embedding_dim)
+        self.linear_c1 = MLP(input_dim=c1_in_channels, embed_dim=embedding_dim)
+
+        self.linear_fuse = ConvModule(
+            c1=embedding_dim * 4,
+            c2=embedding_dim,
+            k=1,
+        )
+
+        self.linear_pred = nn.Conv2d(embedding_dim, num_classes, kernel_size=1)
+        self.dropout = nn.Dropout2d(dropout_ratio)
+
+    def forward(self, inputs):
+        c1, c2, c3, c4 = inputs
+
+        ############## MLP decoder on C1-C4 ###########
+        n, _, h, w = c4.shape
+
+        _c4 = self.linear_c4(c4).permute(0, 2, 1).reshape(n, -1, c4.shape[2], c4.shape[3])
+        _c4 = F.interpolate(_c4, size=c1.size()[2:], mode='bilinear', align_corners=False)
+
+        _c3 = self.linear_c3(c3).permute(0, 2, 1).reshape(n, -1, c3.shape[2], c3.shape[3])
+        _c3 = F.interpolate(_c3, size=c1.size()[2:], mode='bilinear', align_corners=False)
+
+        _c2 = self.linear_c2(c2).permute(0, 2, 1).reshape(n, -1, c2.shape[2], c2.shape[3])
+        _c2 = F.interpolate(_c2, size=c1.size()[2:], mode='bilinear', align_corners=False)
+
+        _c1 = self.linear_c1(c1).permute(0, 2, 1).reshape(n, -1, c1.shape[2], c1.shape[3])
+
+        _c = self.linear_fuse(torch.cat([_c4, _c3, _c2, _c1], dim=1))
+
+        x = self.dropout(_c)
+        x = self.linear_pred(x)
+
+        return x
+
+
+class ViTAE_Window_NoShift_basic(nn.Module):
+    def __init__(self, img_size=224, in_chans=3, stages=4, embed_dims=64, token_dims=64, downsample_ratios=[4, 2, 2, 2], kernel_size=[7, 3, 3, 3], 
+                RC_heads=[1, 1, 1, 1], NC_heads=4, dilations=[[1, 2, 3, 4], [1, 2, 3], [1, 2], [1, 2]],
+                RC_op='cat', RC_tokens_type=['performer', 'transformer', 'transformer', 'transformer'], NC_tokens_type='transformer',
+                RC_group=[1, 1, 1, 1], NC_group=[1, 32, 64, 64], NC_depth=[2, 2, 6, 2], mlp_ratio=4., qkv_bias=True, qk_scale=None, drop_rate=0., 
+                attn_drop_rate=0., drop_path_rate=0., norm_layer=partial(nn.LayerNorm, eps=1e-6), num_classes=1000,
+                gamma=False, init_values=1e-4, SE=False, window_size=7, relative_pos=False):
+        super().__init__()
+        self.num_classes = num_classes
+        self.stages = stages
+        repeatOrNot = (lambda x, y, z=list: x if isinstance(x, z) else [x for _ in range(y)])
+        self.embed_dims = repeatOrNot(embed_dims, stages)
+        self.tokens_dims = token_dims if isinstance(token_dims, list) else [token_dims * (2 ** i) for i in range(stages)]
+        self.downsample_ratios = repeatOrNot(downsample_ratios, stages)
+        self.kernel_size = repeatOrNot(kernel_size, stages)
+        self.RC_heads = repeatOrNot(RC_heads, stages)
+        self.NC_heads = repeatOrNot(NC_heads, stages)
+        self.dilaions = repeatOrNot(dilations, stages)
+        self.RC_op = repeatOrNot(RC_op, stages)
+        self.RC_tokens_type = repeatOrNot(RC_tokens_type, stages)
+        self.NC_tokens_type = repeatOrNot(NC_tokens_type, stages)
+        self.RC_group = repeatOrNot(RC_group, stages)
+        self.NC_group = repeatOrNot(NC_group, stages)
+        self.NC_depth = repeatOrNot(NC_depth, stages)
+        self.mlp_ratio = repeatOrNot(mlp_ratio, stages)
+        self.qkv_bias = repeatOrNot(qkv_bias, stages)
+        self.qk_scale = repeatOrNot(qk_scale, stages)
+        self.drop = repeatOrNot(drop_rate, stages)
+        self.attn_drop = repeatOrNot(attn_drop_rate, stages)
+        self.norm_layer = repeatOrNot(norm_layer, stages)
+        self.relative_pos = relative_pos
+
+        self.pos_drop = nn.Dropout(p=drop_rate)
+        depth = np.sum(self.NC_depth)
+        dpr = [x.item() for x in torch.linspace(0, drop_path_rate, depth)]  # stochastic depth decay rule
+        Layers = []
+        for i in range(stages):
+            startDpr = 0 if i==0 else self.NC_depth[i - 1]
+            Layers.append(
+                BasicLayer(img_size, in_chans, self.embed_dims[i], self.tokens_dims[i], self.downsample_ratios[i],
+                self.kernel_size[i], self.RC_heads[i], self.NC_heads[i], self.dilaions[i], self.RC_op[i],
+                self.RC_tokens_type[i], self.NC_tokens_type[i], self.RC_group[i], self.NC_group[i], self.NC_depth[i], dpr[startDpr:self.NC_depth[i]+startDpr],
+                mlp_ratio=self.mlp_ratio[i], qkv_bias=self.qkv_bias[i], qk_scale=self.qk_scale[i], drop=self.drop[i], attn_drop=self.attn_drop[i],
+                norm_layer=self.norm_layer[i], gamma=gamma, init_values=init_values, SE=SE, window_size=window_size, relative_pos=relative_pos)
+            )
+            img_size = img_size // self.downsample_ratios[i]
+            in_chans = self.tokens_dims[i]
+        self.layers = nn.ModuleList(Layers)
+
+        # Classifier head
+        self.head = nn.Linear(self.tokens_dims[-1], num_classes) if num_classes > 0 else nn.Identity()
+
+        self.apply(self._init_weights)
+        self.decode_head = SegFormerHead(num_classes, [64, 128, 256, 512], 768)
+
+    def freeze_backbone(self):
+        backbone = [self.layers]
+        for module in backbone:
+            try:
+                for param in module.parameters():
+                    param.requires_grad = False
+            except:
+                module.requires_grad = False
+
+    def _init_weights(self, m):
+        if isinstance(m, nn.Linear):
+            trunc_normal_(m.weight, std=.02)
+            if isinstance(m, nn.Linear) and m.bias is not None:
+                nn.init.constant_(m.bias, 0)
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.constant_(m.bias, 0)
+            nn.init.constant_(m.weight, 1.0)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'cls_token'}
+
+    def get_classifier(self):
+        return self.head
+
+    def reset_classifier(self, num_classes):
+        self.num_classes = num_classes
+        self.head = nn.Linear(self.embed_dim, num_classes) if num_classes > 0 else nn.Identity()
+
+    def forward_features(self, x):
+
+        for layer in self.layers:
+            x = layer(x)
+
+        return torch.mean(x, 1)
+
+
+
+    def forward(self, x):
+        # x = self.forward_features(x)
+        # x = self.head(x)
+        H, W = x.size(2), x.size(3)
+
+        temp_outs = []
+        out = []
+        for i, layer in enumerate(self.layers):
+            x = layer(x)
+            temp_outs.append(x)
+
+        temp0 = temp_outs[0].view([-1, int(H)//4, int(H)//4, 64]).permute([0, 3, 1, 2])
+        out.append(temp0)
+        temp1 = temp_outs[1].view([-1, int(H)//8, int(H)//8, 128]).permute([0, 3, 1, 2])
+        out.append(temp1)
+        temp2 = temp_outs[2].view([-1, int(H)//16, int(H)//16, 256]).permute([0, 3, 1, 2])
+        out.append(temp2)
+        temp3 = temp_outs[3].view([-1, int(H)//32, int(H)//32, 512]).permute([0, 3, 1, 2])
+
+        out.append(temp3)
+
+        # -----------------------------------以上是backbone出来的东西-----------------------------------------
+        x = self.decode_head.forward(out)
+        x = F.interpolate(x, size=(H, W), mode='bilinear', align_corners=True)
+
+        return x
+    
+    # def train(self, mode=True, tag='default'):
+    #     r"""Sets the module in training mode.
+
+    #     This has any effect only on certain modules. See documentations of
+    #     particular modules for details of their behaviors in training/evaluation
+    #     mode, if they are affected, e.g. :class:`Dropout`, :class:`BatchNorm`,
+    #     etc.
+
+    #     Args:
+    #         mode (bool): whether to set training mode (``True``) or evaluation
+    #                      mode (``False``). Default: ``True``.
+
+    #     Returns:
+    #         Module: self
+    #     """
+    #     self.training = mode
+    #     if tag == 'default':
+    #         for module in self.children():
+    #             module.train(mode)
+    #     elif tag == 'linear':
+    #         for module in self.children():
+    #             module.eval()
+    #         self.head.train()
+    #     elif tag == 'linearLN':
+    #         for module in self.children():
+    #             module.train(False, tag=tag)
+    #         self.head.train()
+    #     return self
+
+    # def train(self, mode=True, tag='default'):
+    #     self.training = mode
+    #     if tag == 'default':
+    #         for module in self.modules():
+    #             if module.__class__.__name__ != 'ViTAE_Window_NoShift_basic':
+    #                 module.train(mode)
+    #     elif tag == 'linear':
+    #         for module in self.modules():
+    #             if module.__class__.__name__ != 'ViTAE_Window_NoShift_basic':
+    #                 module.eval()
+    #                 for param in module.parameters():
+    #                     param.requires_grad = False
+    #     elif tag == 'linearLNBN':
+    #         for module in self.modules():
+    #             if module.__class__.__name__ != 'ViTAE_Window_NoShift_basic':
+    #                 if isinstance(module, nn.LayerNorm) or isinstance(module, nn.BatchNorm2d):
+    #                     module.train(mode)
+    #                     for param in module.parameters():
+    #                         param.requires_grad = True
+    #                 else:
+    #                     module.eval()
+    #                     for param in module.parameters():
+    #                         param.requires_grad = False
+    #     self.head.train(mode)
+    #     for param in self.head.parameters():
+    #         param.requires_grad = True
+    #     return self
---
+++ ViTAE-main/segmentation/nets/__init__.py
@@ -0,0 +1,5 @@
+from .VitAE import VitAE_v2
+
+get_model_from_name = {
+    "vitae-win"                : VitAE_v2
+}
